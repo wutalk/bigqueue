b6763b532645c90854e84b345d7e3d9d0751413c
wutalk <wu_yaowen@163.com>
2015-05-05 19:12:15 +0800

    add in-memory PET comparison

--- src/test/java/com/leansoft/bigqueue/perf/BigQueuePeTest.java
+++ src/test/java/com/leansoft/bigqueue/perf/BigQueuePeTest.java
@@ -1,9 +1,11 @@
 package com.leansoft.bigqueue.perf;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,7 +16,10 @@ import com.leansoft.bigqueue.IBigQueue;
 
 public class BigQueuePeTest {
 
+    private static final int COUNT = 100 * 1000;
+
     IBigQueue bigQueue;
+    BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<String>(COUNT);
 
     @Before
     public void setUp() throws Exception {
@@ -54,15 +59,23 @@ public class BigQueuePeTest {
         }
     }
 
-    int count = 100 * 1000;
-
     /**
      * <pre>
+     * size: bigqueue-0.7.1-SNAPSHOT.jar 40,302 bytes
+     * 
      * insert 100000 DNs...
-     * used time: 0.36s insert speed: 277777.78 DN/s
+     * used time: 0.35s insert speed: 285714.29 DN/s
      * 
      * poll 100000 DNs...
-     * used time: 0.22s insert speed: 454545.45 DN/s
+     * used time: 0.20s insert speed: 497512.44 DN/s
+     * 
+     * JDK in-memory queue
+     * 
+     * insert 100000 DNs...
+     * used time: 0.06s insert speed: 1818181.82 DN/s
+     * 
+     * poll 100000 DNs...
+     * used time: 0.02s insert speed: 5555555.56 DN/s
      * </pre>
      */
     @Test
@@ -70,13 +83,17 @@ public class BigQueuePeTest {
 
         insert();
         poll();
+
+        System.out.println("\nJDK in-memory queue");
+        insertBq();
+        pollBq();
     }
 
     private void insert() {
         System.out.println();
-        System.out.println("insert " + count + " DNs...");
+        System.out.println("insert " + COUNT + " DNs...");
         long from = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < COUNT; i++) {
             String dn = "PLMN-PLMN/MRBTS-" + i;
             try {
                 bigQueue.enqueue(dn.getBytes());
@@ -88,15 +105,16 @@ public class BigQueuePeTest {
         long to = System.currentTimeMillis();
 
         double dur = (to - from) / 1000.0;
-        double speed = count / dur;
+        double speed = COUNT / dur;
         System.out.format("used time: %.2fs insert speed: %.2f DN/s\r\n", dur, speed);
+        assertEquals(COUNT, bigQueue.size());
     }
 
     private void poll() {
         System.out.println();
-        System.out.println("poll " + count + " DNs...");
+        System.out.println("poll " + COUNT + " DNs...");
         long from = System.currentTimeMillis();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < COUNT; i++) {
             try {
                 String dn = new String(bigQueue.dequeue());
             } catch (IOException e) {
@@ -107,7 +125,41 @@ public class BigQueuePeTest {
         long to = System.currentTimeMillis();
 
         double dur = (to - from) / 1000.0;
-        double speed = count / dur;
+        double speed = COUNT / dur;
         System.out.format("used time: %.2fs insert speed: %.2f DN/s\r\n", dur, speed);
+        assertEquals(0, bigQueue.size());
+    }
+
+    private void insertBq() {
+        System.out.println();
+        System.out.println("insert " + COUNT + " DNs...");
+
+        long from = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; i++) {
+            String dn = "PLMN-PLMN/MRBTS-" + i;
+            blockingQueue.add(dn);
+        }
+        long to = System.currentTimeMillis();
+
+        double dur = (to - from) / 1000.0;
+        double speed = COUNT / dur;
+        System.out.format("used time: %.2fs insert speed: %.2f DN/s\r\n", dur, speed);
+        assertEquals(COUNT, blockingQueue.size());
+    }
+
+    private void pollBq() {
+        System.out.println();
+        System.out.println("poll " + COUNT + " DNs...");
+
+        long from = System.currentTimeMillis();
+        for (int i = 0; i < COUNT; i++) {
+            String dn = blockingQueue.poll();
+        }
+        long to = System.currentTimeMillis();
+
+        double dur = (to - from) / 1000.0;
+        double speed = COUNT / dur;
+        System.out.format("used time: %.2fs insert speed: %.2f DN/s\r\n", dur, speed);
+        assertEquals(0, blockingQueue.size());
     }
 }
